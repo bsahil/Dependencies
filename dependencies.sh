d1e10268dcec9e021180a57f11d1413e8b405edd
bsahil <bsahil012@gmail.com>
2019-01-05 01:28:28 +0530

    added dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+install git
+install snapd
+install code
+sudo apt-get install build essential
+install clipit
+install autojump
+install nightlight
+%install gnome
+install tlp
+install gnome-tweak-tool
+install dash-to-dock
+install dconf editor
+
